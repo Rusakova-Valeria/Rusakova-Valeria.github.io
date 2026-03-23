db392f0eb3fbbb623f78a1497189ce8f22408106
Foxster <a1eks.struchenkov@gmail.com>
2026-03-23 20:47:12 +0300

    Массовое удаление

---
+++ Задание 3/3 задание/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        // Параметры прямоугольника
+        private Rectangle rect;
+        private int rectWidth = 60;
+        private int rectHeight = 40;
+        private Color rectColor = Color.Blue;
+
+        // Параметры движения
+        private int stepY = 5; // шаг движения по вертикали
+        private int direction = 1; // 1 - вниз, -1 - вверх
+        private bool isFlipped = false; // перевернут ли прямоугольник
+
+        // Для обработки нажатия клавиши
+        private bool isRunning = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Начальная позиция - центр формы
+            rect = new Rectangle(
+                (ClientSize.Width - rectWidth) / 2,
+                (ClientSize.Height - rectHeight) / 2,
+                rectWidth,
+                rectHeight
+            );
+
+            // Запускаем таймер
+            timer1.Start();
+
+            // Подписываемся на событие KeyPreview, чтобы форма перехватывала клавиши
+            this.KeyPreview = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!isRunning) return;
+
+            // Движение по вертикали
+            rect.Y += stepY * direction;
+
+            // Проверка достижения границ
+            bool hitBoundary = false;
+
+            if (rect.Y <= 0) // Верхняя граница
+            {
+                rect.Y = 0;
+                direction = 1; // начинаем движение вниз
+                hitBoundary = true;
+            }
+            else if (rect.Y + rect.Height >= ClientSize.Height) // Нижняя граница
+            {
+                rect.Y = ClientSize.Height - rect.Height;
+                direction = -1; // начинаем движение вверх
+                hitBoundary = true;
+            }
+
+            // При достижении границы - переворот и смена цвета
+            if (hitBoundary)
+            {
+                FlipRectangle();
+                ChangeColor();
+            }
+
+            // Перерисовываем форму
+            Invalidate();
+        }
+
+        private void FlipRectangle()
+        {
+            isFlipped = !isFlipped;
+
+            // Меняем ширину и высоту местами (переворот)
+            int temp = rect.Width;
+            rect.Width = rect.Height;
+            rect.Height = temp;
+
+            // Корректируем позицию, чтобы прямоугольник не вылез за границы
+            if (rect.Y + rect.Height > ClientSize.Height)
+            {
+                rect.Y = ClientSize.Height - rect.Height;
+            }
+        }
+
+        private void ChangeColor()
+        {
+            // Генерируем случайный цвет
+            Random rand = new Random();
+            rectColor = Color.FromArgb(rand.Next(256), rand.Next(256), rand.Next(256));
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            // Рисуем прямоугольник
+            using (SolidBrush brush = new SolidBrush(rectColor))
+            {
+                e.Graphics.FillRectangle(brush, rect);
+            }
+
+            // Рисуем рамку вокруг прямоугольника
+            using (Pen pen = new Pen(Color.Black, 2))
+            {
+                e.Graphics.DrawRectangle(pen, rect);
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            // При изменении размера формы проверяем, не вышел ли прямоугольник за границы
+            if (rect.Y + rect.Height > ClientSize.Height)
+            {
+                rect.Y = ClientSize.Height - rect.Height;
+            }
+            if (rect.Y < 0)
+            {
+                rect.Y = 0;
+            }
+
+            // Перерисовываем
+            Invalidate();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Окончание работы при нажатии любой клавиши
+            isRunning = false;
+            timer1.Stop();
+
+            // Показываем сообщение
+            MessageBox.Show("Программа завершена. Нажмите ОК для выхода.", "Завершение",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            // Закрываем форму
+            this.Close();
+        }
+
+        private void настройкиДвиженияToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Создаем и показываем форму настроек
+            using (Form2 settingsForm = new Form2(timer1.Interval, rectColor))
+            {
+                if (settingsForm.ShowDialog() == DialogResult.OK)
+                {
+                    // Применяем настройки
+                    timer1.Interval = settingsForm.Speed;
+                    rectColor = settingsForm.RectColor;
+
+                    // Обновляем отображение скорости в статусной строке
+                    toolStripStatusLabelSpeed.Text = $"{timer1.Interval} ms";
+
+                    // Перерисовываем
+                    Invalidate();
+                }
+            }
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Задание 3/3 задание/WindowsFormsApp1/Form2.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form2
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.labelSpeed = new System.Windows.Forms.Label();
+            this.trackBarSpeed = new System.Windows.Forms.TrackBar();
+            this.labelSpeedValue = new System.Windows.Forms.Label();
+            this.labelColor = new System.Windows.Forms.Label();
+            this.buttonChooseColor = new System.Windows.Forms.Button();
+            this.panelColorPreview = new System.Windows.Forms.Panel();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSpeed)).BeginInit();
+            this.SuspendLayout();
+
+            // labelSpeed
+            this.labelSpeed.AutoSize = true;
+            this.labelSpeed.Location = new System.Drawing.Point(30, 30);
+            this.labelSpeed.Name = "labelSpeed";
+            this.labelSpeed.Size = new System.Drawing.Size(112, 13);
+            this.labelSpeed.TabIndex = 0;
+            this.labelSpeed.Text = "Скорость движения:";
+
+            // trackBarSpeed
+            this.trackBarSpeed.Location = new System.Drawing.Point(30, 60);
+            this.trackBarSpeed.Minimum = 10;
+            this.trackBarSpeed.Maximum = 200;
+            this.trackBarSpeed.Name = "trackBarSpeed";
+            this.trackBarSpeed.Size = new System.Drawing.Size(250, 45);
+            this.trackBarSpeed.TabIndex = 1;
+            this.trackBarSpeed.TickFrequency = 10;
+            this.trackBarSpeed.Scroll += new System.EventHandler(this.trackBarSpeed_Scroll);
+
+            // labelSpeedValue
+            this.labelSpeedValue.AutoSize = true;
+            this.labelSpeedValue.Location = new System.Drawing.Point(290, 65);
+            this.labelSpeedValue.Name = "labelSpeedValue";
+            this.labelSpeedValue.Size = new System.Drawing.Size(37, 13);
+            this.labelSpeedValue.TabIndex = 2;
+            this.labelSpeedValue.Text = "50 ms";
+
+            // labelColor
+            this.labelColor.AutoSize = true;
+            this.labelColor.Location = new System.Drawing.Point(30, 120);
+            this.labelColor.Name = "labelColor";
+            this.labelColor.Size = new System.Drawing.Size(85, 13);
+            this.labelColor.TabIndex = 3;
+            this.labelColor.Text = "Цвет фигуры:";
+
+            // buttonChooseColor
+            this.buttonChooseColor.Location = new System.Drawing.Point(30, 150);
+            this.buttonChooseColor.Name = "buttonChooseColor";
+            this.buttonChooseColor.Size = new System.Drawing.Size(120, 30);
+            this.buttonChooseColor.TabIndex = 4;
+            this.buttonChooseColor.Text = "Выбрать цвет...";
+            this.buttonChooseColor.UseVisualStyleBackColor = true;
+            this.buttonChooseColor.Click += new System.EventHandler(this.buttonChooseColor_Click);
+
+            // panelColorPreview
+            this.panelColorPreview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelColorPreview.Location = new System.Drawing.Point(170, 150);
+            this.panelColorPreview.Name = "panelColorPreview";
+            this.panelColorPreview.Size = new System.Drawing.Size(100, 30);
+            this.panelColorPreview.TabIndex = 5;
+
+            // buttonOK
+            this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonOK.Location = new System.Drawing.Point(100, 220);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(90, 30);
+            this.buttonOK.TabIndex = 6;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+
+            // buttonCancel
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(210, 220);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(90, 30);
+            this.buttonCancel.TabIndex = 7;
+            this.buttonCancel.Text = "Отмена";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+
+            // Form2
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 311);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.panelColorPreview);
+            this.Controls.Add(this.buttonChooseColor);
+            this.Controls.Add(this.labelColor);
+            this.Controls.Add(this.labelSpeedValue);
+            this.Controls.Add(this.trackBarSpeed);
+            this.Controls.Add(this.labelSpeed);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Настройки движения";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSpeed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelSpeed;
+        private System.Windows.Forms.TrackBar trackBarSpeed;
+        private System.Windows.Forms.Label labelSpeedValue;
+        private System.Windows.Forms.Label labelColor;
+        private System.Windows.Forms.Button buttonChooseColor;
+        private System.Windows.Forms.Panel panelColorPreview;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
