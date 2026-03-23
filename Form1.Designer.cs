ad0c2c16d5d9dcc5c06e4c419491bf568ba2ce8f
Валерия <133575285+Rusakova-Valeria@users.noreply.github.com>
2026-03-23 12:09:34 +0300

    3 задание (коды)

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.настройкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.настройкиДвиженияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabelSpeed = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+
+            // timer1
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+
+            // menuStrip1
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+                this.настройкиToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+
+            // настройкиToolStripMenuItem
+            this.настройкиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+                this.настройкиДвиженияToolStripMenuItem,
+                this.выходToolStripMenuItem});
+            this.настройкиToolStripMenuItem.Name = "настройкиToolStripMenuItem";
+            this.настройкиToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.настройкиToolStripMenuItem.Text = "Настройки";
+
+            // настройкиДвиженияToolStripMenuItem
+            this.настройкиДвиженияToolStripMenuItem.Name = "настройкиДвиженияToolStripMenuItem";
+            this.настройкиДвиженияToolStripMenuItem.Size = new System.Drawing.Size(188, 22);
+            this.настройкиДвиженияToolStripMenuItem.Text = "Настройки движения";
+            this.настройкиДвиженияToolStripMenuItem.Click += new System.EventHandler(this.настройкиДвиженияToolStripMenuItem_Click);
+
+            // выходToolStripMenuItem
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(188, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+
+            // statusStrip1
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+                this.toolStripStatusLabel1,
+                this.toolStripStatusLabelSpeed});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+
+            // toolStripStatusLabel1
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(109, 17);
+            this.toolStripStatusLabel1.Text = "Скорость движения:";
+
+            // toolStripStatusLabelSpeed
+            this.toolStripStatusLabelSpeed.Name = "toolStripStatusLabelSpeed";
+            this.toolStripStatusLabelSpeed.Size = new System.Drawing.Size(37, 17);
+            this.toolStripStatusLabelSpeed.Text = "50 ms";
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Движение прямоугольника - Вариант 20";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem настройкиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem настройкиДвиженияToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelSpeed;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        // Свойства для передачи данных обратно в Form1
+        public int Speed { get; private set; }
+        public Color RectColor { get; private set; }
+
+        public Form2(int currentSpeed, Color currentColor)
+        {
+            InitializeComponent();
+
+            // Устанавливаем текущие значения
+            trackBarSpeed.Value = currentSpeed;
+            labelSpeedValue.Text = $"{currentSpeed} ms";
+            panelColorPreview.BackColor = currentColor;
+
+            // Сохраняем начальные значения
+            Speed = currentSpeed;
+            RectColor = currentColor;
+        }
+
+        private void trackBarSpeed_Scroll(object sender, EventArgs e)
+        {
+            labelSpeedValue.Text = $"{trackBarSpeed.Value} ms";
+        }
+
+        private void buttonChooseColor_Click(object sender, EventArgs e)
+        {
+            // Открываем диалог выбора цвета
+            colorDialog1.Color = panelColorPreview.BackColor;
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                panelColorPreview.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            // Сохраняем выбранные значения
+            Speed = trackBarSpeed.Value;
+            RectColor = panelColorPreview.BackColor;
+        }
+    }
+}
